f38afb8464a6b24bf5a8c6b13446a6d1783ee655
Emmanuelbankz <emmio.biz@gmail.com>
2022-09-09 05:09:37 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio>
+
+/**
+*main - print the stringi in the put function
+*
+*Describing: using the main function
+*this program prints "programming is like building a multilingual puzzle
+*Return: 0
+*/
+int main (void)
+{
+	put("\"programming is like building a multilingual puzzle");
+	return (0);
+}
