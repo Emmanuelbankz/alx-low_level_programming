e0583ffdfa96965117cfebbe62a6d870a6a8f34a
Emmanuelbankz <emmio.biz@gmail.com>
2022-09-27 02:23:17 -0700

    task4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,16 +2,16 @@
 #include "2-strlen.c"
 
 /**
- * pint_rev - reverse a string
+ * print_rev - reverse a string
  *
- * @: string to print
+ * @s: string to print
  */
 
 void print_rev(char *s)
 {
 	int i;
 
-	for (i = _strng(s) - 1; i >= 0; i--)
+	for (i = _strlen(s) - 1; i >= 0; i--)
 	{
 		_putchar(*(s + i));
 	}
