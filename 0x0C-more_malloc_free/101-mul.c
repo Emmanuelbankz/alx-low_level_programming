1dc635d8913de425cdfdcd38606586d48561cba6
Emmanuelbankz <emmio.biz@gmail.com>
2022-10-06 22:10:55 -0700

    malloc

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _print - moves a string one place to the left and prints the string
+ * @str: string to move
+ * @l: size of string
+ *
+ * Return: void
+ */
