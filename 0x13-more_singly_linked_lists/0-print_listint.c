01c7bc70e6de593340ddfd3e2d820f7d85ecad83
Emmanuelbankz <emmio.biz@gmail.com>
2022-10-30 06:59:57 -0700

    more

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,13 +3,13 @@
 
 /**
  * print_listint - prints all the elements of a list.
- * @h: head of a list.
- *
+ * @h: head of a list
  * Return: numbers of nodes.
  */
 
 size_t print_listint(const listint_t *h)
 {
+
 	size_t couunt = 0;
 
 	while (h != NULL)
@@ -18,5 +18,7 @@ size_t print_listint(const listint_t *h)
 		h = h->next;/* (h).next*/
 		nn++;
 	}
+
 	return (nn);
+
 }
