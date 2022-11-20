43c93747809fa86be2e47fb70ec4502a08bad826
Emmanuelbankz <emmio.biz@gmail.com>
2022-11-20 12:08:21 -0800

    task

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,19 @@
-#include <stdio.h> 
-  
- /** 
-  * main - Prints the alphabetic in lower and upper case 
-  * 
-  * Return: Always (Success) 
-  */ 
- int main(void) 
- { 
-         char c; 
-  
-         for (c = 'a'; c <= 'z'; c++) 
-         { 
-                 putchar(c); 
-         } 
-  
-         for (c = 'A'; c <= 'Z'; c++) 
-         { 
-                 putchar(c); 
-         } 
-  
-         putchar('\n'); 
-  
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ */
+
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}
