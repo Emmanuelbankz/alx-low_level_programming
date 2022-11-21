789927976f97d748b913c9ed8021bcad16dc91a5
Emmanuelbankz <emmio.biz@gmail.com>
2022-11-20 21:34:31 -0800

    task1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,46 +1,33 @@
-#include <stdio.h> 
-  
- /** 
-   * main - Prints 3 combination of numbers 
-   * 
-   * Return: Always (Success) 
-   */ 
- int main(void) 
- { 
-         int c, i, k, j; 
-  
-         for (c = 48; c <= 57; c++) 
-         { 
-                 for (i = 48; i <= 57; i++) 
-                 { 
-                         for (k = 48; k <= 57; k++) 
-                         { 
-                                 for (j = 48; j <= 57; j++) 
-                                 { 
-                                         if (((k + j) > (c + i) &&  k >= c) || c < k) 
-                                         { 
-                                                 putchar(c); 
-                                                 putchar(i); 
-                                                 putchar(' '); 
-                                                 putchar(k); 
-                                                 putchar(j); 
-  
-                                         if (c + i + k + j == 227 && c == 57) 
-                                         { 
-                                         break; 
-                                         } 
-                                         else 
-                                         { 
-                                         putchar(','); 
-                                         putchar(' '); 
-                                         } 
-                                         } 
-                                 } 
-                         } 
-                 } 
-         } 
-  
-         putchar('\n'); 
-  
-         return (0); 
- }
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ */
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0; i <= 99; i++)
+	{
+		for (j = i + 1; j <= 99; j++)
+		{
+			putchar(i / 10 + '0');
+			putchar(i % 10 + '0');
+			putchar(' ');
+			putchar(j / 10 + '0');
+			putchar(j % 10 + '0');
+
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
